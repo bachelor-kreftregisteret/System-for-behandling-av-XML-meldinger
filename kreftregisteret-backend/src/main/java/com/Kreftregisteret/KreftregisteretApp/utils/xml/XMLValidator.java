4a4c19270980a2536b95655fbe6aa90544d3a0d9
olagberg <s341874@oslomet.no>
2022-01-27 16:10:22 +0100

    Made a new utils.xml package with XMLValidator, which will validate XML files against an XSD

---
+++ kreftregisteret-backend/src/main/java/com/Kreftregisteret/KreftregisteretApp/utils/xml/XMLValidator.java
@@ -0,0 +1,9 @@
+package com.Kreftregisteret.KreftregisteretApp.utils.xml;
+
+public class XMLValidator {
+    private XMLValidator() {}
+
+    public boolean validate() {
+        throw new UnsupportedOperationException("The method is not implemented");
+    }
+}
