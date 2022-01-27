40e8b12bf12474a2cdd5f18d29317d5d9ffde783
olagberg <s341874@oslomet.no>
2022-01-27 17:54:32 +0100

    XMLValidator.validate is implemented. Unsure about if the method should have "File" as parameter, or an other classtype supported by a database.

--- kreftregisteret-backend/src/main/java/com/Kreftregisteret/KreftregisteretApp/utils/xml/XMLValidator.java
+++ kreftregisteret-backend/src/main/java/com/Kreftregisteret/KreftregisteretApp/utils/xml/XMLValidator.java
@@ -1,9 +1,51 @@
 package com.Kreftregisteret.KreftregisteretApp.utils.xml;
 
-public class XMLValidator {
-    private XMLValidator() {}
+import org.xml.sax.SAXException;
 
-    public boolean validate() {
-        throw new UnsupportedOperationException("The method is not implemented");
+import javax.xml.XMLConstants;
+import javax.xml.transform.Source;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import javax.xml.validation.Validator;
+import java.io.File;
+import java.io.IOException;
+
+public class XMLValidator {
+    private XMLValidator() {
+    }
+
+    /*
+      Inspired by
+      https://docs.oracle.com/en/java/javase/17/docs/api/java.xml/javax/xml/validation/package-summary.html
+      The method is not parsing the "File XML" to a "Document" because it will fail with
+      "cvc-elt.1.a: Cannot find the declaration of element" of the root element.
+      Therefor we will use StreamSource with the XML file as an argument in "validator.validate".
+    */
+
+    public static boolean validate(File XML, File XSD) throws IOException, SAXException {
+        // Make SchemaFactory capable of understanding WXS schemas
+        SchemaFactory factory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+
+        // Load WXS schema, represented by a Schema instance
+        Source schemaFile = new StreamSource(XSD);
+        Schema schema = factory.newSchema(schemaFile);
+
+        // create Validator instance
+        Validator validator = schema.newValidator();
+
+        try {
+            validator.validate(new StreamSource(XML));
+            System.out.printf("SUCCESS: %s succesfully validated against %s \n", XML.getName(), XSD.getName());
+
+            // XML file is validated against XSD successfully
+            return true;
+        } catch (SAXException saxException) {
+            System.out.println(saxException.getMessage());
+            System.out.printf("FAIL: %s failed to validate against %s \n", XML.getName(), XSD.getName());
+
+            // XML file failed to validate against XSD file
+            return false;
+        }
     }
 }
