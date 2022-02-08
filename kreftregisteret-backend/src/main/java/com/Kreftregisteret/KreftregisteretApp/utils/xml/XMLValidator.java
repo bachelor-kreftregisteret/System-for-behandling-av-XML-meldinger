20c78093af56b7387754171e7fb64ca19cb72bbd
olagberg <s341874@oslomet.no>
2022-02-09 00:05:12 +0100

    👷 Created XSD_MAP for XSD values and test

--- kreftregisteret-backend/src/main/java/com/Kreftregisteret/KreftregisteretApp/utils/xml/XMLValidator.java
+++ kreftregisteret-backend/src/main/java/com/Kreftregisteret/KreftregisteretApp/utils/xml/XMLValidator.java
@@ -10,8 +10,16 @@ import javax.xml.validation.SchemaFactory;
 import javax.xml.validation.Validator;
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
 
 public class XMLValidator {
+
+    public static final HashMap<String, String> XSD_MAP = new HashMap<>() {{
+        put("Prostata_4_0_KirurgiEksempelfil.xml", "KliniskProstataKirurgi_v4_0.xsd");
+        put("Prostata_4_0_StraalebehandlingEksempelfil.xml", "KliniskProstataStraalebehandling_v4_0.xsd");
+        put("Prostata_4_0_UtredningEksempelfil.xml", "KliniskProstataUtredning_v4_0.xsd");
+    }};
+
     private XMLValidator() {
     }
 
