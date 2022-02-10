2dd9d6c74e6a14948a3564c5642e4efa7e0858b8
olagberg <s341874@oslomet.no>
2022-02-10 15:22:32 +0100

    🔧 generateSchema fixed XSD path

--- kreftregisteret-backend/src/main/java/com/Kreftregisteret/KreftregisteretApp/utils/xml/MessageValidator.java
+++ kreftregisteret-backend/src/main/java/com/Kreftregisteret/KreftregisteretApp/utils/xml/MessageValidator.java
@@ -7,13 +7,14 @@ import javax.xml.XMLConstants;
 import javax.xml.validation.Schema;
 import javax.xml.validation.SchemaFactory;
 import java.io.File;
+import com.Kreftregisteret.KreftregisteretApp.utils.xml.XMLValidator;
 
 public class MessageValidator {
     private static final String XSD_FOLDER = "XSD/";
     private MessageValidator() {}
 
     public static Schema generateSchema(Melding melding) throws SAXException {
-        File XSD = new File(XSD_FOLDER + melding.getSkjemaNavn());
+        File XSD = new File(XSD_FOLDER + XMLValidator.XSD_MAP.get(melding.getSkjemaNavn()));
         SchemaFactory factory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
         Schema schema = factory.newSchema(XSD);
         return schema;
