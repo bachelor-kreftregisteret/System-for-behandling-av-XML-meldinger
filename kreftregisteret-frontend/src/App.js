11fcafd9e2affdd44d179badc35c4afcaee8e90d
Hajin Barzingi <hajin.barzingi@gyldendal.no>
2022-02-16 15:33:38 +0100

    Added method for inserting data from backend into survey.

--- kreftregisteret-frontend/src/App.js
+++ kreftregisteret-frontend/src/App.js
@@ -1,19 +1,13 @@
-import React, {useEffect, useState} from "react";
-import surveyJSON from "./survey/surveyProstataUtredning";
-import * as Survey from "survey-react";
-import DataFetching from "./DataFetching";
-
+import React from "react";
+import SurveyUtredning from "./surveyDisplay/surveyUtredning";
 function App() {
+
   return (
-
       <div className="App">
-        <main>
-          <Survey.Survey
-              json={surveyJSON}
-          />
-            <DataFetching/>
+          <main>
+              <SurveyUtredning/>
+          </main>
 
-        </main>
       </div>
   );
 }
