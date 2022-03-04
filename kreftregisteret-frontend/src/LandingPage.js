b9a57dff8082612c7dc41e03b065491298ea638f
Hajin Barzingi <hajin.barzingi@gyldendal.no>
2022-03-04 14:03:13 +0100

    La til kilde i landingpage for navigation

--- kreftregisteret-frontend/src/LandingPage.js
+++ kreftregisteret-frontend/src/LandingPage.js
@@ -1,6 +1,7 @@
 import React, {useCallback} from 'react';
 import {useNavigate} from 'react-router-dom';
 //https://www.w3schools.com/react/react_router.asp
+//https://stackoverflow.com/questions/29244731/react-router-how-to-manually-invoke-link
 function LandingPage () {
     const navigate = useNavigate();
     const handleOnClick = useCallback(() => navigate('/utredning', {replace: true}), [navigate]);
