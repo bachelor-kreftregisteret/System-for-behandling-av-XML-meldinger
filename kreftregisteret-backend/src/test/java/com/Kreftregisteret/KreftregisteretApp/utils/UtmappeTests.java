9ea984b8d461f8ad5d588a7805e3d7916a9ea098
olagberg <s341874@oslomet.no>
2022-03-07 22:31:30 +0100

    👷 Created test for getPath()

--- kreftregisteret-backend/src/test/java/com/Kreftregisteret/KreftregisteretApp/utils/UtmappeTests.java
+++ kreftregisteret-backend/src/test/java/com/Kreftregisteret/KreftregisteretApp/utils/UtmappeTests.java
@@ -1,5 +1,6 @@
 package com.Kreftregisteret.KreftregisteretApp.utils;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -12,10 +13,18 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 public class UtmappeTests {
     @Test
+    @DisplayName("Test: listFiles()")
     void listFiles() throws MalformedURLException {
         File[] files = Utmappe.listFiles();
         assertNotNull(files);
         // Contains a file that ends with KliniskProstataUtredning.xml
         assertTrue(Arrays.toString(files).contains("KliniskProstataUtredning.xml"));
+    }
+
+    @Test
+    @DisplayName("Test: getPath()")
+    void getPath() throws MalformedURLException {
+        String path = Utmappe.getPath();
+        assertTrue(path.contains("utmappe"));
     }
 }
