6f3a5fd632375cfeaeb5d55c6a5ebf530f331745
olagberg <s341874@oslomet.no>
2022-03-08 14:28:11 +0100

    🔧⚠️Deleted patch method, comments and print messages due to clutter

--- kreftregisteret-backend/src/main/java/com/Kreftregisteret/KreftregisteretApp/controllers/MeldingController.java
+++ kreftregisteret-backend/src/main/java/com/Kreftregisteret/KreftregisteretApp/controllers/MeldingController.java
@@ -1,22 +1,16 @@
 package com.Kreftregisteret.KreftregisteretApp.controllers;
 
-
 import com.Kreftregisteret.KreftregisteretApp.models.Melding;
 import com.Kreftregisteret.KreftregisteretApp.utils.xml.MessageManager;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.github.fge.jsonpatch.JsonPatch;
-import com.google.gson.Gson;
 import jakarta.xml.bind.JAXBException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.xml.sax.SAXException;
-
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.TransformerException;
 import java.io.IOException;
 import java.util.HashMap;
-
 
 @RestController
 public class MeldingController {
@@ -39,49 +33,20 @@ public class MeldingController {
         return messageManager.findMeldingById(id);
     }
 
-    //nå skal vi legge til en metode som gjør det mulig å hente alle meldinger ELLER bare metadata om de..
-    //dvs at best practice er faktisk å lage en
 
-    //http://localhost:8080/api/v1/meldinger
     @CrossOrigin(origins = {"http://localhost:8080", "http://localhost:3000", "http://localhost:3001"})
     @GetMapping(path = "api/v1/meldinger")
     public HashMap<Melding, Long> getAllMeldinger() throws IOException {
         //Kanskje vi bør vi ved lokasjonen til hver ID istedenfor ID? eller begge?
-
         return messageManager.getMsgMap();
     }
 
-    @PatchMapping(
-            path = "/api/v1/meldinger",
-            consumes = "application/json-patch+json")
-    public ResponseEntity<Melding> updateMelding(@RequestBody JsonPatch patch) {
-        //todo legg til parsing av json patch her.
-        Gson gson = new Gson();
-        Melding object = gson.fromJson(String.valueOf(patch), Melding.class);
-        System.out.println(object);
-        return ResponseEntity.ok(object);
-    }
 
-
-    //i førstee omgang bør klienten sende hele json filen!! Så får vi se om vi klarer å lage patch senere...
     @CrossOrigin(origins = {"http://localhost:8080", "http://localhost:3000", "http://localhost:3001"})
-    @PostMapping(
-            path = "/api/v1/meldinger",
-            consumes = "application/json")
+    @PostMapping(path = "/api/v1/meldinger", consumes = "application/json")
     public ResponseEntity<Melding> postMelding(@RequestBody Melding melding) throws JAXBException, ParserConfigurationException, IOException, ClassNotFoundException, TransformerException, SAXException {
-        //vi får inn en "hel melding" her, så må vi se om vi klarer å direkte lagre en xml ut i fra dette
-        //Melding melding = meldingService.
-        //validering
-        ObjectMapper mapper = new ObjectMapper();
-
-        //Object to JSON in String
-        String jsonInString = mapper.writeValueAsString(melding);
-        System.out.println(jsonInString);
-
-        System.out.println(melding);
-
         try {
-            MessageManager.writeMeldingToPath(melding);
+            MessageManager.writeMeldingToPath(melding); // Validation happens here
             return ResponseEntity.ok(null);
         } catch (SAXException | JAXBException e) {
             // XML validation failed. Write error logic here:
@@ -89,6 +54,5 @@ public class MeldingController {
             return ResponseEntity.ok(null);
         }
     }
-
 }
 
