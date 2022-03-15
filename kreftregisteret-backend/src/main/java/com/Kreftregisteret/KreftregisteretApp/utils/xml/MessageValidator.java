2be8dff2ba864b3118725f770a6647bc5f0f0847
olagberg <s341874@oslomet.no>
2022-03-15 22:39:11 +0100

    🔧 Trying to use InputStream and TempFiles to reach resources in MessageValidator

--- kreftregisteret-backend/src/main/java/com/Kreftregisteret/KreftregisteretApp/utils/xml/MessageValidator.java
+++ kreftregisteret-backend/src/main/java/com/Kreftregisteret/KreftregisteretApp/utils/xml/MessageValidator.java
@@ -1,7 +1,8 @@
 package com.Kreftregisteret.KreftregisteretApp.utils.xml;
 
+import com.Kreftregisteret.KreftregisteretApp.KreftregisteretAppApplication;
 import com.Kreftregisteret.KreftregisteretApp.models.Melding;
-import org.springframework.core.io.ClassPathResource;
+import com.Kreftregisteret.KreftregisteretApp.utils.StreamUtil;
 import org.xml.sax.SAXException;
 
 import javax.xml.XMLConstants;
@@ -9,15 +10,12 @@ import javax.xml.validation.Schema;
 import javax.xml.validation.SchemaFactory;
 import java.io.File;
 import java.io.IOException;
-import com.Kreftregisteret.KreftregisteretApp.utils.xml.XMLValidator;
 
 public class MessageValidator {
     private MessageValidator() {}
 
     public static Schema generateSchema(Melding melding) throws SAXException, IOException {
-        final String PROSTATAPAKKE = new ClassPathResource("Prostatapakke").getURL().getPath() + "/";
-
-        File XSD = new File(PROSTATAPAKKE + XMLValidator.XSD_MAP.get(melding.getSkjemaNavn()));
+        File XSD = StreamUtil.stream2file(KreftregisteretAppApplication.class.getClassLoader().getResourceAsStream("XSD/" + XMLValidator.XSD_MAP.get(melding.getSkjemaNavn())));
         SchemaFactory factory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
         Schema schema = factory.newSchema(XSD);
         return schema;
