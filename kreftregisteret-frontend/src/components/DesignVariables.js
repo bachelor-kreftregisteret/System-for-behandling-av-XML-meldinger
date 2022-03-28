1dffc3f9d2411c81ed39e71618b5a4c80ea448d6
Hajin Barzingi <hajin.barzingi@gyldendal.no>
2022-03-28 16:32:21 +0200

    Minor design change - frontpage

--- kreftregisteret-frontend/src/components/DesignVariables.js
+++ kreftregisteret-frontend/src/components/DesignVariables.js
@@ -1,4 +1,5 @@
 export const Color = {
-    king_blue: "#21314D"
+    king_blue: "#21314D",
+    turquoise: "#6BA4B8"
 
 }
--- kreftregisteret-frontend/src/components/Header.js
+++ kreftregisteret-frontend/src/components/Header.js
@@ -1,4 +1,5 @@
 import {Outlet, Link} from "react-router-dom";
+import {Color} from "./DesignVariables";
 
 const Header = () => {
 
@@ -6,8 +7,8 @@ const Header = () => {
         <>
             <nav>
                 <ul>
-                    <li style={{listStyle: "none", margin: "0", padding:"0"}}>
-                        <Link to="/">Forside</Link>
+                    <li style={{listStyle: "none", marginTop: "0.5em", padding:"0"}}>
+                        <Link style={{textDecoration: "none", color: Color.turquoise, fontWeight: "bold"}} to="/"> ⫷ Til meldingsoversikt </Link>
                     </li>
                 </ul>
             </nav>
