fc1b71c095704ea520959673f5d0c2f6b5f10467
HajinB <hajinbarzingi@gmail.com>
2022-04-09 13:57:19 +0200

    Minor design footer and hover.

--- kreftregisteret-frontend/src/components/Footer.js
+++ kreftregisteret-frontend/src/components/Footer.js
@@ -1,7 +1,14 @@
 import React from "react";
 import "./css/footer.css"
+import {useNavigate} from "react-router-dom";
 
 const Footer = (props) => {
+    const navigate = useNavigate();
+
+    //https://stackoverflow.com/questions/50644976/react-button-onclick-redirect-page
+    const routeChange = (url) =>{
+        navigate(url);
+    }
 
     return (
         <footer
@@ -19,6 +26,7 @@ const Footer = (props) => {
                         <button
                             type={"reset"}
                             className={"footerBtn cancelBtn"}
+                            onClick={() => routeChange("/")}
                         >Avslutt</button>
                         <button
                             type={"submit"}
